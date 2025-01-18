963a8121e6c7ec0facc36b3d6e41d1c863a14195
f1ar3 <goshaapolonoff@yandex.ru>
2025-01-18 21:15:53 +0300

    feat: создать CustomUserManage для управления модели пользователя

---
+++ notes-app-backend/users/managers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import UserManager
+
+class CustomUserManager(UserManager):
+    def _create_user(self, username, email, password, **extra_fields):
+        if not email:
+            raise ValueError('The email must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username=None, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username=None, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self._create_user(username, email, password, **extra_fields)
